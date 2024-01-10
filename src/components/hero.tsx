59e26b0404d3adb33c9378ea36783202897c9edb
wamiti8711@gmail.com <wamiti8711@gmail.com>
2024-01-10 15:15:00 +0300

    Feat: hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,5 @@
-import Image, { StaticImageData } from 'next/image';
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
 
 interface HeroProps {
   imgData: StaticImageData;
@@ -13,9 +14,10 @@ export default function Hero(props: HeroProps) {
         <Image
           src={props.imgData}
           alt={props.imgAlt}
-          layout="fill"
-          objectFit="cover"
+          fill
+          style={{ objectFit: 'cover' }}
         />
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900" />
       </div>
       <div className="pt-48 flex justify-center items-center">
         <h1 className="text-white text-6xl">{props.title}</h1>
