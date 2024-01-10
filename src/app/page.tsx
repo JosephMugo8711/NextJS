29401709938ee329f02726623a825ed618f36a83
wamiti8711@gmail.com <wamiti8711@gmail.com>
2024-01-10 10:48:30 +0300

    Feat: Linking between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
      <div>
-       Hello World!
+       <div>
+         <Link  href="/performance">Performance</Link>
+         <Link  href="/reliability">Reliability</Link>
+         <Link  href="/scale">Scale</Link>
+       </div>
+       Home Page
      </div>
   )
 }
